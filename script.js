f9c3e167dd476ddc719f552ba0a479ab625339e1
Francis <wayncis2@gmail.com>
2022-04-27 21:48:45 +0300

    Add user interface class

--- script.js
+++ script.js
@@ -11,7 +11,53 @@ class Book {
   }
 }
 
+//Ui class - handle ui tasks
+class UserInterface {
+  static updateBrowser(book) {
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book-holder');
+    // add book title
+    const titleP = document.createElement('p');
+    titleP.textContent = book.title;
+    // add author
+    const authorP = document.createElement('p');
+    authorP.textContent = book.author;
+    // add hidden id field
+    const bookId = document.createElement('p');
+    bookId.setAttribute('type', 'hidden');
+    bookId.setAttribute('value', book.id);
+    //  create delete button
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Remove';
+    deleteBtn.classList.add('deleteBtn');
+    const line = document.createElement('hr');
+    bookDiv.appendChild(titleP);
+    bookDiv.appendChild(authorP);
+    bookDiv.appendChild(bookId);
+    bookDiv.appendChild(deleteBtn);
+    bookDiv.appendChild(line);
+    bookList.appendChild(bookDiv);
+  }
 
+  static displayBooks(){
+    const books =  localStorage.getItem('books');
+    if(books){
+      bookStore = JSON.parse(books);
+      bookStore.forEach(book => UserInterface.updateBrowser(book));
+    }else{
+      localStorage.setItem('books', JSON.stringify(bookStore));
+    }
+  }
+
+  static addBook(book) {
+    if (book.title !== '' || book.author !== '') {
+      bookStore.push(book);
+      localStorage.setItem('books', JSON.stringify(bookStore));
+      // add book to browser
+      UserInterface.updateBrowser(book);
+    }
+  }
+}
 
 
 
