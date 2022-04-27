2d307a84b237fae9450a9bf051d204cfff385880
Francis <wayncis2@gmail.com>
2022-04-27 16:17:03 +0300

    Add funtion to store books in array

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const formSubmit = document.querySelector('.submitBtn');
 const forms = document.forms;
 const bookList = document.querySelector('.book-list');
-
 let bookStore = [];
+
+
+// add books to the bookStore array as an object
+function addBook(bookTitle, bookAuthor) {
+  if(bookTitle === '' || bookAuthor === '') {
+    console.log('Fill in all fields')
+  }else {
+    const book = {
+      title: bookTitle,
+      writter: bookAuthor,
+      id: new Date().valueOf()
+    }
+    bookStore.push(book);
+    localStorage.setItem('books', JSON.stringify(bookStore));
+    // add book to browser
+    UpdateBrowser(book)
+  }
+}
